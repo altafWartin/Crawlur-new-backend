ece23d98f3742d1e0318dab7edbadfdd66a916c1
altafWartin <altaf@wartinlabs.com>
2024-10-09 18:23:59 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,4 +7,11 @@ const router = express.Router();
 router.post('/create-User', createUser);  // Only Admins can create Analysts
 router.get('/users', authenticate, authorize('Admin'), listUsers);  // Only Admins can list users
 
+// Analyst routes
+// router.post('/create-analyst', authenticate, authorize('Admin'), createUser);
+// router.delete('/delete-analyst/:id', authenticate, authorize('Admin'), deleteUser);
+// router.put('/update-analyst/:id', authenticate, authorize('Admin'), updateUser);
+// router.get('/analyst/:id', authenticate, authorize('Admin'), getUserById);
+
+
 module.exports = router;
